ec18896b89104c6f7826a445ce98f188261ab1fa
KelvinGodinho21 <43483941+KelvinGodinho21@users.noreply.github.com>
2018-09-26 06:36:38 +1000

    Update 123.py

--- 123.py
+++ 123.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import random
 
-description = '''css
+description = '''
 Welcome to The Help Command Index
 
 Please See A Staff Member If you have any other problems!
@@ -32,13 +32,13 @@ async def fortnite (ctx):
         myline = random.choice(lines)
     split = myline.partition(":")
     
-    embed=discord.Embed(title="Fornite Account", color=0xff00b6)
+    embed=discord.Embed(title="StrafeGen - Fornite Account", color=0xff00b6)
     embed.set_thumbnail(url="https://png.icons8.com/color/1600/fortnite.png")
     embed.add_field(name="Email:", value=split[0], inline=True)
     embed.add_field(name="Password:", value=split[2], inline=False)
     await bot.send_message(ctx.message.author, embed=embed)
 
-    print("{} Typed !gen fortnite".format(userName))
+    print("{} Typed !fortnite".format(userName))
     
 lines = open(r'Spotify.txt').read().splitlines()
 
@@ -51,13 +51,13 @@ async def spotify (ctx):
         myline = random.choice(lines)
     split = myline.partition(":")
     
-    embed=discord.Embed(title="Spotify Account", color=0x00ff00)
+    embed=discord.Embed(title="StrafeGen - Spotify Account", color=0x00ff00)
     embed.set_thumbnail(url="https://image.flaticon.com/icons/png/512/232/232413.png")
     embed.add_field(name="Email:", value=split[0], inline=True)
     embed.add_field(name="Password:", value=split[2], inline=False)
     await bot.send_message(ctx.message.author, embed=embed)
 
-    print("{} Typed !gen spotify".format(userName))
+    print("{} Typed !spotify".format(userName))
 
 lines = open(r'Netflix.txt').read().splitlines()
 
@@ -70,7 +70,7 @@ async def netflix (ctx):
         myline = random.choice(lines)
     split = myline.partition(":")
     
-    embed=discord.Embed(title="Netflix Account", color=0xf00000)
+    embed=discord.Embed(title="StrafeGen - Netflix Account", color=0xf00000)
     embed.set_thumbnail(url="https://mbtskoudsalg.com/images/netflix-icon-png-3.png")
     embed.add_field(name="Email:", value=split[0], inline=True)
     embed.add_field(name="Password:", value=split[2], inline=False)
@@ -89,7 +89,7 @@ async def pornhub (ctx):
         myline = random.choice(lines)
     split = myline.partition(":")
     
-    embed=discord.Embed(title="Pornhub Account", color=0xffff00)
+    embed=discord.Embed(title="StrafeGen - Pornhub Account", color=0xffff00)
     embed.set_thumbnail(url="https://mbtskoudsalg.com/image/pornhub-logo-png/740542.html")
     embed.add_field(name="Email:", value=split[0], inline=True)
     embed.add_field(name="Password:", value=split[2], inline=False)
@@ -108,7 +108,7 @@ async def hulu (ctx):
         myline = random.choice(lines)
     split = myline.partition(":")
     
-    embed=discord.Embed(title="Hulu Account", color=0x80ff00)
+    embed=discord.Embed(title="StrafeGen - Hulu Account", color=0x80ff00)
     embed.set_thumbnail(url="https://mbtskoudsalg.com/images250_/hulu-icon.png")
     embed.add_field(name="Email:", value=split[0], inline=True)
     embed.add_field(name="Password:", value=split[2], inline=False)
@@ -127,7 +127,7 @@ async def minecraft (ctx):
         myline = random.choice(lines)
     split = myline.partition(":")
     
-    embed=discord.Embed(title="Minecraft Account", color=0x2ffd6c)
+    embed=discord.Embed(title="StrafeGen - Minecraft Account", color=0x2ffd6c)
     embed.set_thumbnail(url="https://pre00.deviantart.net/2640/th/pre/f/2016/133/0/d/minecraft_hd_logo_by_nuryrush-da2aumi.png")
     embed.add_field(name="Email:", value=split[0], inline=True)
     embed.add_field(name="Password:", value=split[2], inline=False)
